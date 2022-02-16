3a322f001609b3c1d10fce09105ee6742216ec44
jimmy0251 <jimmy0251@gmail.com>
2022-02-17 05:13:05 +0530

    Initial commit

---
+++ Sources/UIPilot/UIPilot.swift
@@ -0,0 +1,6 @@
+public struct UIPilot {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/UIPilotTests/UIPilotTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import UIPilot
+
+final class UIPilotTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(UIPilot().text, "Hello, World!")
+    }
+}
