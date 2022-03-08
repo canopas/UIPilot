81b0746e29f1ca208fe4089d8b42caa37804330a
amisha <amisha.i@canopas.com>
2022-03-08 18:43:33 +0530

    Add podspec file

--- Tests/UIPilotTests/UIPilotTests.swift
+++ Tests/UIPilotTests/UIPilotTests.swift
@@ -6,6 +6,5 @@ final class UIPilotTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(UIPilot().text, "Hello, World!")
     }
 }
